aa468cf4343450830fa9e492364c851b9648bb57
david-owens-24 <david.owens24@live.com>
2025-08-04 14:27:19 +0100

    updated employee overview to use nested EmployeeCard component instead of a table

---
+++ PieShopHRM/Components/EmployeeCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace PieShopHRM.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = new Employee();
+    }
+}
