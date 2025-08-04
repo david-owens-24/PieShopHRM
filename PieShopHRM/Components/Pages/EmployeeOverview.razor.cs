1f36aaba47697c5758fe79fff22e09410c83e50c
david-owens-24 <david.owens24@live.com>
2025-08-04 13:46:31 +0100

    added mock data service and start of EmployeeOverview component

---
+++ PieShopHRM/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using PieShopHRM.Services;
+using System.Runtime.CompilerServices;
+
+namespace PieShopHRM.Components.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employees { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
