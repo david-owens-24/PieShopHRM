e359118e00568e3fe4a73026e4207639a8a1d76e
david-owens-24 <david.owens24@live.com>
2025-08-04 14:00:16 +0100

    added EmployeeDetail component

---
+++ PieShopHRM/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using PieShopHRM.Services;
+
+namespace PieShopHRM.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = MockDataService.Employees?.FirstOrDefault(e => e.EmployeeId == EmployeeId);
+        }
+    }
+}
